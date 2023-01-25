13c440c30768441877c19269aac722001f8162a0
Ermachenkov Maxim <epmak.8383@mail.ru>
2023-01-25 22:56:18 +0300

    создание программы

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿string[] array = { "быстро", "дешево", "ура", "качественно", "2" };
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        Console.WriteLine(array[i]);
+    }
+}
